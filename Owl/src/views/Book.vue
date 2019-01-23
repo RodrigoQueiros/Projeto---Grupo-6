f06250ef70595e26b7292d13511c502546694b95
RodrigoQueiros <rodrigodlq@hotmail.com>
2019-01-23 16:12:46 +0000

    ds

--- Owl/src/views/Book.vue
+++ Owl/src/views/Book.vue
@@ -453,7 +453,7 @@ export default {
             "/" +
             (currentDate.getMonth() + 1) +
             "/" +
-            currentDate.getFullYear() +
+            currentDate.getFullYear() + " " +
             currentDate.getHours() +
             ":" +
             currentDate.getMinutes(),
