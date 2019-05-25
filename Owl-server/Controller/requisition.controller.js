084118d798f561ba81d4421fa873f4fd9f299538
AFGRocha <antoniolaranjinha@hotmail.com>
2019-05-25 13:46:03 +0100

    API (cont)

--- Owl-server/Controller/requisition.controller.js
+++ Owl-server/Controller/requisition.controller.js
@@ -2,8 +2,13 @@
 const Requistion = require('../Models/requisition.model.js')
 
 async function get(req, res) {
+    const id = req.query
     try {
-        return res.send(await Requistion.find())
+        if (id.userId) {
+            return res.send(await Requistion.find({userId: id.userId}))
+        } else {
+            return res.status(400).send({ error: `Could not get books: ${err}` })
+        }
     } catch (err) {
         return res.status(400).send({ error: `Could not get books: ${err}` })
     }
--- Owl-server/Controller/review.controller.js
+++ Owl-server/Controller/review.controller.js
@@ -2,13 +2,46 @@
 const Review = require('../Models/review.model.js')
 
 async function get(req, res) {
+    const id = req.query
+    console.log(id)
     try {
-        return res.send(await Review.find())
+        if (id.bookId){
+            console.log("true")
+            return res.send(await Review.find({bookId: id.bookId}))
+        } else {
+            return res.status(400).send({ error: `Could not get reviews: id not defined` })
+        }
+        
     } catch (err) {
         return res.status(400).send({ error: `Could not get reviews: ${err}` })
     }
 }
+/*
+async function get(req, res) {
+    const { email } = req.query
+    console.log("teste")
+    try {
+        if (email) {
+            console.log("email")
+            const response = await User.findOne({ email })
+            if (response) {
+                return res.send(response)
+            } else {
+                return res.status(404).send({ error: "Email not found" })
+            }
+        } else {
+            console.log("no email")
+            try {
+                return res.send(await User.find())
+            } catch (err) {
+                return res.status(400).send({ error: err })
+            }
+        }
 
+    } catch (err) {
+        return res.status(400).send({ error: `Could not get users: ${err}` })
+    }
+}*/
 
 async function post(req, res) {
     try {
