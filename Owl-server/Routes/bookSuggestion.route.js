4599e43bd7eade7039287027762b5aef7665dcc9
AFGRocha <antoniolaranjinha@hotmail.com>
2019-06-08 14:18:16 +0100

    +1 Suggestion

--- Owl-server/Routes/bookSuggestion.route.js
+++ Owl-server/Routes/bookSuggestion.route.js
@@ -4,6 +4,7 @@ const controller = require("../Controller/bookSuggestion.controller")
 
 router.get("/", controller.get)
 router.post("/", controller.post)
+router.put("/:id", controller.put)
 router.delete("/:id", controller.del)
 
 module.exports = app => app.use("/bookSuggestions", router)
